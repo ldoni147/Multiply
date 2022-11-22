29acb3ff28e5de5377b772f0ef4e272caf297c8c
Lainey Donisch <ldoni147@d127.org>
2022-11-22 09:13:56 -0600

    Initial Commit

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var answerUIImageView: UIImageView!
+    
+    @IBOutlet weak var answerLable: UILabel!
+    
+    @IBOutlet weak var number1UITextField: UITextField!
+    
+    @IBOutlet weak var number2UITextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func equalWhenPressed(_ sender: UIButton) {
+        //MVP - Kennedy
+        
+        
+        //Stretch 2 & 3
+        
+        
+        //Stretch 1 (check Display a Number Hackwich) - Lainey
+        
+    }
+    
 }
 
